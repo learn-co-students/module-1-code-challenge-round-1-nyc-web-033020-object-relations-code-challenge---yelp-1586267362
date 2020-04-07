ba9012a14529a8f57c0ffe6c87163b7f64c875d8
dkersten <dkersten1994@gmail.com>
2020-04-07 10:55:31 -0400

    First commit

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,40 @@
 class Restaurant
   attr_reader :name
+  
+  @@all = []
 
   def initialize(name)
     @name = name
+
+    Restaurant.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    reviews.map do |review|
+      review.customer
+    end.uniq
+  end
+
+  def average_star_rating
+    total = 0
+    num_of_reviews = 0
+
+    arr_of_reviews = reviews.map do |review|
+      review.rating
+    end
+    num_of_reviews = arr_of_reviews.length
+    total = arr_of_reviews.reduce(0) {|sum, n| sum + n}
+    total / num_of_reviews
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+customer1 = Customer.new("Shawn", "Spencer")
+customer2 = Customer.new("Burton", "Guster")
+customer3 = Customer.new("Juliet", "O'Hara")
+customer4 = Customer.new("Carton", "Lassiter")
+customer5 = Customer.new("Henry", "Spencer")
+customer6 = Customer.new("Shawn", "White")
+
+restaurant1 = Restaurant.new("Del Taco")
+restaurant2 = Restaurant.new("Japadogs")
+restaurant3 = Restaurant.new("Wendy's")
+restaurant4 = Restaurant.new("McDonald's")
+restaurant5 = Restaurant.new("Burger King")
+
+review1 = Review.new(customer1, restaurant2, 5)
+review2 = Review.new(customer2, restaurant2, 4)
+review3 = Review.new(customer2, restaurant1, 3)
+review4 = Review.new(customer4, restaurant4, 5)
+review5 = Review.new(customer5, restaurant2, 2)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
