9486a79d446cdf45c1918b587427f2def57884c7
mishyjari <mishyjari@outlook.com>
2020-04-07 10:19:49 -0400

    initial boilerplate

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer
+  @@all = []
   attr_accessor :given_name, :family_name
 
   def initialize(given_name, family_name)
@@ -9,4 +10,29 @@ class Customer
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def restaurants
+    
+  end
+
+  def add_review restaurant rating
+
+  end
+
+  def num_reviews
+
+  end
+
+  def self.find_by_name fullname
+
+  end
+
+  def self.find_by_given_name
+
+  end
+
+  def self.all 
+    @@all
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,21 @@
 class Restaurant
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
+    Restaurant.all << self
   end
 
+  def reviews
+
+  end
+
+  def customers
+
+  end
+
+  def self.all
+    @@all
+  end
 end
